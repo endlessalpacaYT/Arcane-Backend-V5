58dd663b1427f64f1cb8c087d222bfd5621266c3
PongoDev <90353497+endlessalpacaYT@users.noreply.github.com>
2024-12-25 00:33:38 +0000

    Update stats.js

--- routes/FN-Service/stats.js
+++ routes/FN-Service/stats.js
@@ -2,41 +2,77 @@ async function stats(fastify, options) {
     fastify.get('/fortnite/api/stats/accountId/:accountId/bulk/window/:windowId', (request, reply) => {
         reply.status(200).send([
             {
-                "name": "br_score_pc_m0_p10",
-                "value": 0,
-                "window": "alltime",
-                "ownerType": 1
+                name: "br_placetop6_pc_m0_p9",
+                value: 1,
             },
             {
-                "name": "br_score_pc_m0_p2",
-                "value": 0,
-                "window": "alltime",
-                "ownerType": 1
+                name: "br_placetop3_pc_m0_p9",
+                value: 2,
             },
             {
-                "name": "br_kills_pc_m0_p2",
-                "value": 0,
-                "window": "alltime",
-                "ownerType": 1
+                name: "br_matchesplayed_pc_m0_p9",
+                value: 3,
             },
             {
-                "name": "br_matchesplayed_pc_m0_p2",
-                "value": 0,
-                "window": "alltime",
-                "ownerType": 1
+                name: "br_placetop1_pc_m0_p9",
+                value: 4,
             },
             {
-                "name": "br_kills_pc_m0_p9",
-                "value": 0,
-                "window": "alltime",
-                "ownerType": 1
+                name: "br_kills_pc_m0_p9",
+                value: 5,
             },
             {
-                "name": "br_minutesplayed_pc_m0_p2",
-                "value": 0,
-                "window": "alltime",
-                "ownerType": 1
-            }
+                name: "br_minutesplayed_pc_m0_p9",
+                value: 6,
+            },
+            {
+                name: "br_placetop12_pc_m0_p10",
+                value: 7,
+            },
+            {
+                name: "br_placetop5_pc_m0_p10",
+                value: 8,
+            },
+            {
+                name: "br_matchesplayed_pc_m0_p10",
+                value: 9,
+            },
+            {
+                name: "br_placetop1_pc_m0_p10",
+                value: 10,
+            },
+            {
+                name: "br_kills_pc_m0_p10",
+                value: 11,
+            },
+            {
+                name: "br_minutesplayed_pc_m0_p10",
+                value: 12,
+            },
+            {
+                name: "br_placetop25_pc_m0_p2",
+                value: 13,
+            },
+            {
+                name: "br_placetop10_pc_m0_p2",
+                value: 14,
+            },
+            {
+                name: "br_placetop1_pc_m0_p2",
+                value: 15,
+            },
+            {
+                name: "br_kills_pc_m0_p2",
+                value: 16,
+            },
+            {
+                name: "br_matchesplayed_pc_m0_p2",
+                value: 17,
+            },
+            {
+                name: "br_minutesplayed_pc_m0_p2",
+                value: 18,
+            },
         ])
     })
 
