51e7fe1474ce26b4ca8a855d8e9df3cc0a8d0cfe
PongoDev <90353497+endlessalpacaYT@users.noreply.github.com>
2024-12-27 00:07:16 +0000

    api v1

---
+++ routes/api/v1.js
@@ -0,0 +1,22 @@
+async function v1(fastify, options) {
+    fastify.get('/v1/launcher/servers/status', (request, reply) => {
+        reply.status(200).send({
+            "SERVER_STATUS": "offline",
+            "LAUNCHER_STATUS": "offline"
+        })
+    })
+
+    fastify.get('/v1/launcher/update/fetch', (request, reply) => {
+        reply.status(200).send({
+            "updatelink": "https://launcher.evolvefn.com/1.0.0"
+        })
+    })
+
+    fastify.get('/v1/launcher/update/currentversion', (request, reply) => {
+        reply.status(200).send({
+            "LAUNCHER_VERSION": "1.0.0"
+        })
+    })
+}
+
+module.exports = v1;
