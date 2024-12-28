02108743e0fb47fe8dc796eda12ede4f7ec4662f
PongoDev <90353497+endlessalpacaYT@users.noreply.github.com>
2024-12-28 03:13:51 +0000

    Update user.js

--- database/models/user.js
+++ database/models/user.js
@@ -26,6 +26,7 @@ const UserSchema = new mongoose.Schema({
     },
     security: {
         password: { type: String, required: true },
+        banned: { type: Boolean, default: false }
     },
     privacySettings: {
         type: Object,
