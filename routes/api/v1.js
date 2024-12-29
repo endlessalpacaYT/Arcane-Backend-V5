4def6f5b058ebbe63ddf5435fed7e3e0bd8d5991
PongoDev <90353497+endlessalpacaYT@users.noreply.github.com>
2024-12-29 00:02:02 +0000

    Update v1.js

--- routes/api/v1.js
+++ routes/api/v1.js
@@ -1,20 +1,20 @@
 async function v1(fastify, options) {
     fastify.get('/v1/launcher/servers/status', (request, reply) => {
         reply.status(200).send({
-            "SERVER_STATUS": "offline",
-            "LAUNCHER_STATUS": "offline"
+            "SERVER_STATUS": "online",
+            "LAUNCHER_STATUS": "online"
         })
     })
 
     fastify.get('/v1/launcher/update/fetch', (request, reply) => {
         reply.status(200).send({
-            "updatelink": "https://cdn.evolvefn.com/launcher/EvolveLauncher_1.0.13_EN_US.exe"
+            "updatelink": "https://cdn.evolvefn.com/launcher/EvolveLauncher_1.0.14_en-US.exe"
         })
     })
 
     fastify.get('/v1/launcher/update/currentversion', (request, reply) => {
         reply.status(200).send({
-            "LAUNCHER_VERSION": "1.0.1"
+            "LAUNCHER_VERSION": "1.0.2"
         })
     })
 }
