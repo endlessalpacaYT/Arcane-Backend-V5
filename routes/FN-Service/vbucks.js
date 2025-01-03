66ba456d46dcbdec9c196e23b1b95b3d55e7ca16
PongoDev <90353497+endlessalpacaYT@users.noreply.github.com>
2025-01-03 16:02:21 +0000

    vbucks api works
    we litterally have everything now

---
+++ routes/FN-Service/vbucks.js
@@ -0,0 +1,60 @@
+require("dotenv").config();
+
+const Profile = require("../../database/models/profile.js");
+
+const errors = require("../../responses/errors.json");
+const createError = require("../../utils/error.js");
+const tokenVerify = require("../../middlewares/tokenVerify.js");
+
+async function vbucks(fastify, options) {
+    fastify.post('/fortnite/api/v1/profile/:accountId/vbucks/add', { preHandler: tokenVerify }, async (request, reply) => {
+        const { accountId } = request.params;
+        const { reason } = request.body;
+
+        const profiles = await Profile.findOne({ accountId: accountId });
+        if (!profiles) {
+            return createError.createError(errors.NOT_FOUND.account.not_found, 404, reply);
+        }
+        const profile = profiles.profiles["common_core"];
+
+        let statChanged = false;
+
+        const beforeChanges = profile.items["Currency:MtxPurchased"].quantity;
+
+        try {
+            if (reason == "kill") {
+                profile.items["Currency:MtxPurchased"].quantity += 10;
+                statChanged = true;
+            } else if (reason == "win") {
+                profile.items["Currency:MtxPurchased"].quantity += 40;
+                statChanged = true;
+            } else {
+                console.warn(`Unexpected Vbucks Reason: ${reason}`);
+                profile.items["Currency:MtxPurchased"].quantity += 5;
+                statChanged = true;
+            }
+        } catch (err) {
+            console.error(`Error In Vbucks: ${err}`);
+            profile.items["Currency:MtxPurchased"].quantity += 5;
+            statChanged = true;
+        }
+
+        if (statChanged) {
+            profile.rvn += 1;
+            profile.commandRevision += 1;
+            profile.updated = new Date().toISOString();
+
+            await profiles.updateOne({ $set: { [`profiles.common_core`]: profile } });
+        }
+
+        const afterChanges = profile.items["Currency:MtxPurchased"].quantity;
+
+        reply.status(200).send({
+            status: "success",
+            beforeChanges: beforeChanges,
+            afterChanges: afterChanges
+        })
+    })
+}
+
+module.exports = vbucks;
