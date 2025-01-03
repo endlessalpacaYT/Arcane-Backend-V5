92806fc4d73f95bac7a9dea9c4837efed3ebb1e0
PongoDev <90353497+endlessalpacaYT@users.noreply.github.com>
2025-01-03 15:49:10 +0000

    prevention for users using other seasons

--- routes/FN-Service/calender.js
+++ routes/FN-Service/calender.js
@@ -2,12 +2,18 @@ require("dotenv").config();
 const functions = require("../../utils/functions");
 const keychain = require("../../responses/fortniteConfig/catalog/keychain.json");
 
+const errors = require("../../responses/errors.json");
+const createError = require("../../utils/error.js");
+
 const dailyEnd = new Date(Date.now() + 86400 * 1000).toISOString();
 const weeklyEnd = new Date(Date.now() + 604800 * 1000).toISOString();
 
 async function calender(fastify, options) {
     fastify.get('/fortnite/api/calendar/v1/timeline', (request, reply) => {
         const memory = functions.GetVersionInfo(request);
+        if (memory.season != Number(process.env.SEASON)) {
+            return createError.createError(errors.BAD_REQUEST.Bad_version, 400, reply);
+        }
 
         const now = new Date();
 
