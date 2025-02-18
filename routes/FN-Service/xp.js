627be0c6aa8ecb11c8b1aa7f87e9c4e7e4c015fe
PongoDev <90353497+endlessalpacaYT@users.noreply.github.com>
2025-02-18 20:03:45 +0000

    better xp for c2

--- routes/FN-Service/xp.js
+++ routes/FN-Service/xp.js
@@ -59,13 +59,97 @@ async function xp(fastify, options) {
         }
         
         while (!statChanged) {
-            if (xp > levelXP) {
+            if (xp >= levelXP) {
                 level++;
                 profile.stats.attributes.level += 1;
                 profile.stats.attributes.accountLevel++;
                 book_xp = book_xp + levelBook;
                 profile.stats.attributes.book_xp += levelBook;
-                if (book_xp > 10) {
+                if (book_xp >= 10) {
+                    book_xp = book_xp - 10;
+                    profile.stats.attributes.book_xp -= 10;
+                    book_level++;
+                    profile.stats.attributes.book_level += 1;
+                }
+                xp = xp - levelXP;
+                profile.stats.attributes.xp -= levelXP;
+            } else {
+                statChanged = true;
+            }
+        }
+
+        level++;
+        if (statChanged) {
+            profile.rvn += 1;
+            profile.commandRevision += 1;
+            profile.updated = new Date().toISOString();
+
+            await profiles.updateOne({ $set: { [`profiles.athena`]: profile } });
+        }
+
+        const afterChanges = {
+            level: level,
+            xp: xp,
+            book_xp: book_xp,
+            book_level: book_level,
+            accountLevel: profile.stats.attributes.accountLevel
+        }
+
+        reply.status(200).send({
+            status: "success",
+            beforeChanges: beforeChanges,
+            afterChanges: afterChanges
+        })
+    })
+
+    fastify.post('/fortnite/api/v1/profile/:accountId/xp/addSpecificAmount', async (request, reply) => {
+        const { accountId } = request.params;
+        const { amount, apiKey } = request.body;
+
+        const profiles = await Profile.findOne({ accountId: accountId });
+        if (!profiles) {
+            return createError.createError(errors.NOT_FOUND.account.not_found, 404, reply);
+        }
+        if (!apiKey || apiKey != process.env.JWT_SECRET) {
+            return reply.status(403).send()
+        }
+        
+        const profile = profiles.profiles["athena"];
+
+        let statChanged = false;
+
+        let level = profile.stats.attributes.level - 1;
+        let xp = profile.stats.attributes.xp;
+        let book_xp = profile.stats.attributes.book_xp;
+        let book_level = profile.stats.attributes.book_level;
+        let leveldata = xpMap.levels[level];
+        let levelXP = xpMap.levels[level].xpRequired;
+        let levelBook = xpMap.levels[level].book_xp;
+
+        const beforeChanges = {
+            level: level + 1,
+            xp: xp,
+            book_xp: book_xp,
+            book_level: book_level,
+            accountLevel: profile.stats.attributes.accountLevel
+        }
+
+        try {
+            xp = xp + amount;
+            profile.stats.attributes.xp += amount;
+        } catch (err) {
+            console.error(`Error In XP: ${err}`);
+            xp = xp + 10;
+        }
+        
+        while (!statChanged) {
+            if (xp >= levelXP) {
+                level++;
+                profile.stats.attributes.level += 1;
+                profile.stats.attributes.accountLevel++;
+                book_xp = book_xp + levelBook;
+                profile.stats.attributes.book_xp += levelBook;
+                if (book_xp >= 10) {
                     book_xp = book_xp - 10;
                     profile.stats.attributes.book_xp -= 10;
                     book_level++;
