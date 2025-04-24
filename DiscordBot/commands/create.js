a09969ee7d874a17fb2a04c0be8911dfb835bce0
Marcel <105155313+mxrc3l@users.noreply.github.com>
2025-04-24 21:08:13 +0200

    Update Discord.js and commands

--- DiscordBot/commands/create.js
+++ DiscordBot/commands/create.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require("discord.js");
+const { EmbedBuilder, ApplicationCommandOptionType } = require("discord.js");
 const fs = require("fs");
 const path = require("path");
 const bcrypt = require("bcrypt");
@@ -31,101 +31,105 @@ async function createProfile(accountId) {
     return profile;
 }
 
-module.exports = {
-    commandInfo: {
-        name: "create",
-        description: "Create an account!",
-        options: [
-            {
-                name: "email",
-                description: "Your email.",
-                type: 3,
-                required: true
-            },
-            {
-                name: "password",
-                description: "Your password.",
-                required: true,
-                type: 3
-            },
-            {
-                name: "username",
-                description: "Your username.",
-                type: 3,
-                required: true
-            }
-        ],
-    },
-    execute: async (interaction) => {
-        try {
-            const { options } = interaction;
+const commandInfo = {
+    name: "create",
+    description: "Create an account!",
+    options: [
+        {
+            name: "email",
+            description: "Your email.",
+            type: ApplicationCommandOptionType.String,
+            required: true
+        },
+        {
+            name: "password",
+            description: "Your password.",
+            required: true,
+            type: ApplicationCommandOptionType.String
+        },
+        {
+            name: "username",
+            description: "Your username.",
+            type: ApplicationCommandOptionType.String,
+            required: true
+        }
+    ],
+};
 
-            const discordId = interaction.user.id;
-            const email = options.get("email").value;
-            const username = options.get("username").value;
-            const password = options.get("password").value;
+async function execute(interaction) {
+    try {
+        const { options } = interaction;
 
-            const existingUser = await User.findOne({ 'accountInfo.id': discordId });
-            if (existingUser) {
-                const embed = new MessageEmbed()
-                    .setColor("#ff0000") // Red
-                    .setTitle("Account Already Exists!")
-                    .setDescription("An account already exists with this Discord ID!")
-                    .setTimestamp();
-                await interaction.reply({
-                    embeds: [embed],
-                    ephemeral: true,
-                });
-                return;
-            }
+        const discordId = interaction.user.id;
+        const email = options.get("email").value;
+        const username = options.get("username").value;
+        const password = options.get("password").value;
 
-            const hashedPass = await bcrypt.hash(password, 10);
-
-            const newUser = new User({
-                accountInfo: {
-                    id: discordId,
-                    displayName: username,
-                    email: email,
-                    company: username
-                },
-                security: {
-                    password: hashedPass
-                },
-                privacySettings: {
-                    accountId: discordId,
-                },
-                stash: {
-                    "globalcash": 0
-                }
-            });
-            await newUser.save();
-            createProfile(discordId);
-
-            const friends = new Friends({
-                created: new Date(),
-                accountId: discordId,
-            });
-            await friends.save();
-
-            const embed = new MessageEmbed()
-                .setColor("#a600ff") // Purple
-                .setTitle("Success!")
-                .setDescription(`Account created with username: ${username}!`)
-                .setTimestamp();
-
-            await interaction.reply({ embeds: [embed], ephemeral: true });
-        } catch (error) {
-            console.error('Error executing ping command:', error);
-            const embed = new MessageEmbed()
+        const existingUser = await User.findOne({ 'accountInfo.id': discordId });
+        if (existingUser) {
+            const embed = new EmbedBuilder()
                 .setColor("#ff0000") // Red
-                .setTitle("An Error Occurred!")
-                .setDescription("Please Report This To The Staff/Developers!")
+                .setTitle("Account Already Exists!")
+                .setDescription("An account already exists with this Discord ID!")
                 .setTimestamp();
-
             await interaction.reply({
                 embeds: [embed],
                 ephemeral: true,
             });
+            return;
         }
+
+        const hashedPass = await bcrypt.hash(password, 10);
+
+        const newUser = new User({
+            accountInfo: {
+                id: discordId,
+                displayName: username,
+                email: email,
+                company: username
+            },
+            security: {
+                password: hashedPass
+            },
+            privacySettings: {
+                accountId: discordId,
+            },
+            stash: {
+                "globalcash": 0
+            }
+        });
+        await newUser.save();
+        createProfile(discordId);
+
+        const friends = new Friends({
+            created: new Date(),
+            accountId: discordId,
+        });
+        await friends.save();
+
+        const embed = new EmbedBuilder()
+            .setColor("#a600ff") // Purple
+            .setTitle("Success!")
+            .setDescription(`Account created with username: ${username}!`)
+            .setTimestamp();
+
+        await interaction.reply({ embeds: [embed], ephemeral: true });
+    } catch (error) {
+        console.error('Error executing create command:', error);
+        const embed = new EmbedBuilder()
+            .setColor("#ff0000") // Red
+            .setTitle("An Error Occurred!")
+            .setDescription("Please Report This To The Staff/Developers!")
+            .setTimestamp();
+
+        await interaction.reply({
+            embeds: [embed],
+            ephemeral: true,
+        });
     }
 }
+
+module.exports = {
+    commandInfo,
+    execute
+};
